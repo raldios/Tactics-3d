0f4bd872f8cb7962939041c356e9b5577003e50c
Adaline Hearst <adalinehearst@gmail.com>
2024-03-18 20:33:55 -0400

    initial commit

---
+++ components/health_component.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+var max_health: int = 100
+var current_health: int
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	current_health = max_health
+	pass # Replace with function body.
+
+func set_current_health(target_health: int):
+	if (0 <= target_health) and (target_health <= max_health):
+		current_health = target_health
+	elif target_health < 0:
+		lgg.wrn(["Attempted to set <character> health to less than zero, setting to zero instead."])
+		current_health = 0
+	else:
+		lgg.wrn(['Attempted to set <character> health to more than max, setting to max_health: ', max_health, ' instead.'])
+		current_health = max_health
+
+func get_current_health():
+	return current_health
+	
+func mod_current_health(modifier: int):
+	current_health += modifier
+	
+	if current_health < 0:
+		lgg.wrn(["Attempted to set <character> health to less than zero, setting to zero instead."])
+		current_health = 0
+	elif current_health > max_health:
+		lgg.wrn(['Attempted to set <character> health to more than max, setting to max_health: ', max_health, ' instead.'])
+		current_health = max_health
+		
